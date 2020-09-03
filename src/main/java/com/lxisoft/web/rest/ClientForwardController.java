2c9fff04dae23d4e31741650988c5b44f4f71860
megha-vt <megha.v.t@lxisoft.com>
2020-09-03 13:27:54 +0530

    http://jira.bytatech.com:8080/browse/IN-9937

--- src/main/java/com/lxisoft/web/rest/ClientForwardController.java
+++ src/main/java/com/lxisoft/web/rest/ClientForwardController.java
@@ -2,6 +2,11 @@ package com.lxisoft.web.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lxisoft.model.ExamModel;
 
 @Controller
 public class ClientForwardController {
@@ -14,4 +19,16 @@ public class ClientForwardController {
     public String forward() {
         return "forward:/";
     }
+
+    @GetMapping(value="/instruction")
+    public String root() {return "instruction";}
+    
+    @RequestMapping(value = "/exams", method = RequestMethod.GET)
+    public ModelAndView newContact(ModelAndView model) {
+        ExamModel examModel = new ExamModel();
+        model.addObject("examModel", examModel);
+        model.setViewName("add");
+        return model;
+    }
+
 }
