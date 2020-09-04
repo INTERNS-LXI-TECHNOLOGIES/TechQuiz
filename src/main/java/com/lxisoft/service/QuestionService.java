de2d6494b57b33df123ad94569a914d49e194d69
user <amritha.p.m@lxisoft.com>
2020-09-04 16:28:46 +0530

    create add method

--- src/main/java/com/lxisoft/service/QuestionService.java
+++ src/main/java/com/lxisoft/service/QuestionService.java
@@ -1,7 +1,10 @@
 package com.lxisoft.service;
 
+import com.lxisoft.domain.Question;
+import com.lxisoft.repository.QuestionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +14,28 @@ public class QuestionService {
 
     private final Logger log = LoggerFactory.getLogger(QuestionService.class);
 
+    @Autowired
+    QuestionRepository questionRepository;
+
+    public void saveQuestion(Question question)
+    {
+        questionRepository.save(question);
+    }
+/*    public Question get(long id)
+    {
+        return questionRepository.getOne(id);
+    }
+
+    public List<Question> getAll()
+    {
+
+        return  questionRepository.findAll();
+    }
+
+    public long deleteById(long id)
+    {
+        questionRepository.deleteById(id);
+        return id;
+    }
+*/
 }
--- src/main/java/com/lxisoft/web/rest/ClientForwardController.java
+++ src/main/java/com/lxisoft/web/rest/ClientForwardController.java
@@ -1,5 +1,11 @@
 package com.lxisoft.web.rest;
 
+import com.lxisoft.domain.Answer;
+import com.lxisoft.domain.QnOption;
+import com.lxisoft.domain.Question;
+import com.lxisoft.model.ExamModel;
+import com.lxisoft.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +15,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
 import com.lxisoft.domain.Exam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 public class ClientForwardController {
+    @Autowired
+    private QuestionService questionService;
 
     /**
      * Forwards any unmapped paths (except those containing a period) to the client {@code index.html}.
@@ -31,6 +42,33 @@ public class ClientForwardController {
     	model.addAttribute("exam",exam);
 		return "createxam";
 	}
+    @GetMapping(value = "/createxam")
+    public String addNewQuestion(@ModelAttribute ExamModel examModel){
+        List<QnOption> qnOptions = new ArrayList<>();
+        Question question = examModel.getQuestion();
+        Answer answer = examModel.getAnswer();
+        answer.setQuestion(question);
+        question.setAnswer(answer);
 
+        QnOption option1 = new QnOption();
+        QnOption option2 = new QnOption();
+        QnOption option3 = new QnOption();
+        QnOption option4 = new QnOption();
+        option1.setOption(examModel.getOption1());
+        option2.setOption(examModel.getOption2());
+        option3.setOption(examModel.getOption3());
+        option4.setOption(examModel.getOption4());
+        option1.setQuestion(question);
+        option2.setQuestion(question);
+        option3.setQuestion(question);
+        option4.setQuestion(question);
+        qnOptions.add(option1);
+        qnOptions.add(option2);
+        qnOptions.add(option3);
+        qnOptions.add(option4);
+       // question.setOptions(qnOptions);
+        questionService.saveQuestion(question);
+        return "home";
+    }
 
     }
